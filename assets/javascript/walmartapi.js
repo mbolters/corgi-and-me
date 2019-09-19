407f586bfaeab1e913b9dee3b38d6dcbed967d0a
Thomas Mylott <mylottiv@gmail.com>
2019-09-19 14:24:52 -0500

    Wrapped code inside initShopping function to be called from handler.js

--- assets/javascript/walmartapi.js
+++ assets/javascript/walmartapi.js
@@ -1,74 +1,71 @@
-//start jQuery
-$(document).ready(function(){
-    $("#target1").css("color", "red");
+function initShopping(){
+	//this is the Walmart API search 
+	let walmartLink = "https://www.walmart.com/ip/";
+	let productID = ["417529705", "161250671", "15580429"]
 
-//this is the Walmart API search 
-let walmartLink = "https://www.walmart.com/ip/";
-let productID = ["417529705", "161250671", "15580429"]
-
-var settings = [{
-	"async": true,
-	"crossDomain": true,
-	"url": "https://feeditem-walmart.p.rapidapi.com/itemID/" + productID[0],
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "feeditem-walmart.p.rapidapi.com",
-		"x-rapidapi-key": "290be0fd14mshdbddbf002f394adp1bd091jsn91fedda5b4f3"
-	}
-},
-{
-	"async": true,
-	"crossDomain": true,
-	"url": "https://feeditem-walmart.p.rapidapi.com/itemID/" + productID[1],
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "feeditem-walmart.p.rapidapi.com",
-		"x-rapidapi-key": "290be0fd14mshdbddbf002f394adp1bd091jsn91fedda5b4f3"
-	}
-},
-{
-	"async": true,
-	"crossDomain": true,
-	"url": "https://feeditem-walmart.p.rapidapi.com/itemID/" + productID[2],
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "feeditem-walmart.p.rapidapi.com",
-		"x-rapidapi-key": "290be0fd14mshdbddbf002f394adp1bd091jsn91fedda5b4f3"
-	}
-}];
+	var settings = [{
+		"async": true,
+		"crossDomain": true,
+		"url": "https://feeditem-walmart.p.rapidapi.com/itemID/" + productID[0],
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "feeditem-walmart.p.rapidapi.com",
+			"x-rapidapi-key": "290be0fd14mshdbddbf002f394adp1bd091jsn91fedda5b4f3"
+		}
+	},
+	{
+		"async": true,
+		"crossDomain": true,
+		"url": "https://feeditem-walmart.p.rapidapi.com/itemID/" + productID[1],
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "feeditem-walmart.p.rapidapi.com",
+			"x-rapidapi-key": "290be0fd14mshdbddbf002f394adp1bd091jsn91fedda5b4f3"
+		}
+	},
+	{
+		"async": true,
+		"crossDomain": true,
+		"url": "https://feeditem-walmart.p.rapidapi.com/itemID/" + productID[2],
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "feeditem-walmart.p.rapidapi.com",
+			"x-rapidapi-key": "290be0fd14mshdbddbf002f394adp1bd091jsn91fedda5b4f3"
+		}
+	}];
 
 
-$.ajax(settings[0]).done(function (response) {
-    $("#spinner").hide(); //hides the spinner once results load
-    let price = response.Item.CurrentPrice.Value;
-    $("#firstLink").attr("href", walmartLink + productID[0]);
-    $( "<img>" ).addClass("imageSize").attr( "src", response.Item.PictureURL[2]).appendTo( "#firstLink" );
-    $("#price1").append("$" + price);
+	$.ajax(settings[0]).done(function (response) {
+		$("#spinner").hide(); //hides the spinner once results load
+		let price = response.Item.CurrentPrice.Value;
+		$("#firstLink").attr("href", walmartLink + productID[0]);
+		$( "<img>" ).addClass("imageSize").attr( "src", response.Item.PictureURL[2]).appendTo( "#firstLink" );
+		$("#price1").append("$" + price);
 
 
 
-});
+	});
 
 
 
-$.ajax(settings[1]).done(function (response) {
-    $("#spinner").hide(); //hides the spinner once results load
-    let price = response.Item.CurrentPrice.Value;
-    $("#secondLink").attr("href", walmartLink + productID[1]);
-    $( "<img>" ).addClass("imageSize").attr( "src", response.Item.PictureURL[0]).appendTo( "#secondLink" );
-    $("#price2").append("$" + price);
-});
+	$.ajax(settings[1]).done(function (response) {
+		$("#spinner").hide(); //hides the spinner once results load
+		let price = response.Item.CurrentPrice.Value;
+		$("#secondLink").attr("href", walmartLink + productID[1]);
+		$( "<img>" ).addClass("imageSize").attr( "src", response.Item.PictureURL[0]).appendTo( "#secondLink" );
+		$("#price2").append("$" + price);
+	});
 
 
-$.ajax(settings[2]).done(function (response) {
-    $("#spinner").hide(); //hides the spinner once results load
-    let price = response.Item.CurrentPrice.Value;
-    $("#thirdLink").attr("href", walmartLink + productID[2]);
-    $( "<img>" ).addClass("imageSize").attr( "src", response.Item.PictureURL[1]).appendTo( "#thirdLink" );
-    $("#price3").append("$" + price);
-});
-});
-//Get 'er done Marie
-    //create about page and link
-    //select items per weather condition to display
-    //adorable corgi images for weather
+	$.ajax(settings[2]).done(function (response) {
+		$("#spinner").hide(); //hides the spinner once results load
+		let price = response.Item.CurrentPrice.Value;
+		$("#thirdLink").attr("href", walmartLink + productID[2]);
+		$( "<img>" ).addClass("imageSize").attr( "src", response.Item.PictureURL[1]).appendTo( "#thirdLink" );
+		$("#price3").append("$" + price);
+	});
+}
+	//Get 'er done Marie
+		//create about page and link
+		//select items per weather condition to display
+		//adorable corgi images for weather
